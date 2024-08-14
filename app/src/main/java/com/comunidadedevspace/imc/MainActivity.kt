0fbbb2e2c9c8c7a504265f375c23d50e4b26e316
TatiPiazzoli <tatianamenpia@gmail.com>
2024-08-14 19:38:38 +0100

    Mostrar mensagem preencha todos os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -17,14 +18,28 @@ class MainActivity : AppCompatActivity() {
 
         btnCalcular.setOnClickListener {
 
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
 
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
+            if (pesoStr == "" || alturaStr == ""){
 
-            println("Tatiana ação do botão" + resultado)
-            
+                Snackbar.make(
+
+                    edtPeso,
+                    "Preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+
+                ).show()
+
+            } else {
+
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+                println("Tatiana ação do botão" + resultado)
+            }
         }
     }
 }
