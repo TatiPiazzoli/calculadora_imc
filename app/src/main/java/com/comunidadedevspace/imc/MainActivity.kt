53c2917a292550d5a3dd75422e31e603cc07c2eb
TatiPiazzoli <tatianamenpia@gmail.com>
2024-08-19 20:19:22 +0100

    Mostra resultado e classificacao na próxima tela

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -38,6 +40,11 @@ class MainActivity : AppCompatActivity() {
 
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra (KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
                 println("Tatiana ação do botão" + resultado)
             }
         }
